bebc770e2678f31d0d1b4a035bee4f68c94eb041
Shubham Kumar <87669020+shubhamkv@users.noreply.github.com>
2023-06-01 18:11:25 +0530

    Added solution - LeetHub

--- Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
+++ Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
@@ -8,26 +8,40 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:
-    int f(int ind, int arr[], vector<int> &dp)
-    {
-        if(ind==0)
-          return arr[ind];
-        if(ind<0)
-          return 0;
+    // int f(int ind, int arr[], vector<int> &dp)
+    // {
+    //     if(ind==0)
+    //       return arr[ind];
+    //     if(ind<0)
+    //       return 0;
         
-        if(dp[ind]!=-1)
-          return dp[ind];
+    //     if(dp[ind]!=-1)
+    //       return dp[ind];
           
-        int pick = arr[ind] + f(ind-2,arr,dp);
-        int notPick = 0 + f(ind-1,arr,dp);
+    //     int pick = arr[ind] + f(ind-2,arr,dp);
+    //     int notPick = 0 + f(ind-1,arr,dp);
         
-        return dp[ind]=max(pick,notPick);
-    }
+    //     return dp[ind]=max(pick,notPick);
+    // }
 	// calculate the maximum sum with out adjacent
 	int findMaxSum(int *arr, int n) {
 	    // code here
-	    vector<int> dp(n,-1);
-	    return f(n-1,arr,dp);
+	   // vector<int> dp(n,-1);
+	   // return f(n-1,arr,dp);
+	   
+	   // Tabulation in DP
+	   
+	   vector<int> dp(n,0);
+	   dp[0]=arr[0];
+	   for(int i=1;i<n;i++)
+	   {
+	       int pick = arr[i];
+	       if(i>1)
+	          pick+=dp[i-2];
+	       int notPick = 0 + dp[i-1];
+	       dp[i] = max(pick,notPick);
+	   }
+	   return dp[n-1];
 	}
 };
 
