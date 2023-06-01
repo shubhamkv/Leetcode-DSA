6d04e088204ad88ec1fbecdef83d0b02552275b4
Shubham Kumar <87669020+shubhamkv@users.noreply.github.com>
2023-06-01 18:16:56 +0530

    Added solution - LeetHub

--- Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
+++ Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
@@ -28,20 +28,38 @@ public:
 	    // code here
 	   // vector<int> dp(n,-1);
 	   // return f(n-1,arr,dp);
+	   //--------------------------------------------------------------------------------
 	   
 	   // Tabulation in DP
 	   
+	   //vector<int> dp(n,0);
+	   //dp[0]=arr[0];
+	   //for(int i=1;i<n;i++)
+	   //{
+	   //    int pick = arr[i];
+	   //    if(i>1)
+	   //       pick+=dp[i-2];
+	   //    int notPick = 0 + dp[i-1];
+	   //    dp[i] = max(pick,notPick);
+	   //}
+	   //return dp[n-1];
+	   
+	   //-----------------------------Space Optimization----------------------------------
+	   
 	   vector<int> dp(n,0);
-	   dp[0]=arr[0];
+	   int prev=arr[0];
+	   int prev2=0;
 	   for(int i=1;i<n;i++)
 	   {
 	       int pick = arr[i];
 	       if(i>1)
-	          pick+=dp[i-2];
-	       int notPick = 0 + dp[i-1];
-	       dp[i] = max(pick,notPick);
+	          pick+=prev2;
+	       int notPick = 0 + prev;
+	       int curr = max(pick,notPick);
+	       prev2 = prev;
+	       prev = curr;
 	   }
-	   return dp[n-1];
+	   return prev;
 	}
 };
 
