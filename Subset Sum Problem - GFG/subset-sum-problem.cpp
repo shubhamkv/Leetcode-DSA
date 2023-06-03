e140103fbab7e628f78cd0db330a8d36ce01f547
Shubham Kumar <87669020+shubhamkv@users.noreply.github.com>
2023-06-03 12:46:01 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -8,30 +8,55 @@ using namespace std;
 //User function template for C++
 
 class Solution{   
-private:
-    bool f(int ind, int sum, vector<int> &arr, vector<vector<int>> &dp)
-    {
-        if(sum==0)
-          return true;
-        if(ind==0)
-          return arr[ind]==sum;
+    
+// ---------------Memoization DP Solution----------------------------------------
+
+// private:
+//     bool f(int ind, int sum, vector<int> &arr, vector<vector<int>> &dp)
+//     {
+//         if(sum==0)
+//           return true;
+//         if(ind==0)
+//           return arr[ind]==sum;
         
-        if(dp[ind][sum]!=-1)
-           return dp[ind][sum];
+//         if(dp[ind][sum]!=-1)
+//           return dp[ind][sum];
            
-        bool notTake = f(ind-1,sum,arr,dp);
-        bool take = false;
-        if(sum>=arr[ind])
-          take = f(ind-1,sum-arr[ind],arr,dp);
+//         bool notTake = f(ind-1,sum,arr,dp);
+//         bool take = false;
+//         if(sum>=arr[ind])
+//           take = f(ind-1,sum-arr[ind],arr,dp);
           
-        return dp[ind][sum] = (take || notTake) ;
-    }
+//         return dp[ind][sum] = (take || notTake) ;
+//     }
+
 public:
     bool isSubsetSum(vector<int>arr, int sum){
         // code here 
         int n = arr.size();
-        vector<vector<int>> dp(n, vector<int>(sum+1,-1));
-        return f(n-1,sum,arr,dp);
+        // vector<vector<int>> dp(n, vector<int>(sum+1,-1));
+        // return f(n-1,sum,arr,dp);
+        
+        // --------------------Tabulation DP Solution---------------------
+        
+        vector<vector<bool>> dp(n, vector<bool>(sum+1,0));
+        for(int i=0;i<n;i++)
+          dp[i][0] = true;
+        dp[0][arr[0]] = true;
+        
+        for(int i=1;i<n;i++)
+        {
+            for(int target=1;target<=sum;target++)
+            {
+                bool notTake = dp[i-1][target];
+                bool take = false;
+                if(target>=arr[i])
+                   take = dp[i-1][target-arr[i]];
+          
+                dp[i][target] = (take || notTake) ;
+            }
+        }
+        return dp[n-1][sum];
     }
 };
 
