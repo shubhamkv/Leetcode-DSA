51d87273db81543579046d3a824dba048a12908b
Shubham Kumar <87669020+shubhamkv@users.noreply.github.com>
2023-06-10 17:03:36 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -8,30 +8,55 @@ using namespace std;
 // User function Template for C++
 
 class Solution{
-private:
-    int f(int ind, int W, int wt[], int val[], vector<vector<int>> &dp)
-    {
-        if(ind==0)
-        {
-            return int(W/wt[0])*val[0];
-        }
+    // --------------Memoization DP Solution---------------------------------
+// private:
+//     int f(int ind, int W, int wt[], int val[], vector<vector<int>> &dp)
+//     {
+//         if(ind==0)
+//         {
+//             return int(W/wt[0])*val[0];
+//         }
         
-        if(dp[ind][W]!=-1)
-          return dp[ind][W];
+//         if(dp[ind][W]!=-1)
+//           return dp[ind][W];
         
-        int notTake = 0 + f(ind-1,W,wt,val,dp);
-        int take = INT_MIN;
-        if(wt[ind]<=W)
-          take = val[ind] + f(ind,W-wt[ind],wt,val,dp);
+//         int notTake = 0 + f(ind-1,W,wt,val,dp);
+//         int take = INT_MIN;
+//         if(wt[ind]<=W)
+//           take = val[ind] + f(ind,W-wt[ind],wt,val,dp);
            
-        return dp[ind][W] = max(notTake,take);
-    }
-public:
+//         return dp[ind][W] = max(notTake,take);
+//     }
+ public:
     int knapSack(int N, int W, int val[], int wt[])
     {
         // code here
-        vector<vector<int>> dp(N, vector<int> (W+1,-1));
-        return f(N-1,W,wt,val,dp);
+        // vector<vector<int>> dp(N, vector<int> (W+1,-1));
+        // return f(N-1,W,wt,val,dp);
+        
+        // -----------------------Tabulation DP Solution-------------------------
+        
+        vector<vector<int>> dp(N, vector<int> (W+1,0));
+        
+        for(int weight=0;weight<=W;weight++)
+        {
+            dp[0][weight] = int(weight/wt[0]) * val[0];
+        }
+        
+        for(int ind=1;ind<N;ind++)
+        {
+            for(int weight=0;weight<=W;weight++)
+            {
+                int notTake = 0 + dp[ind-1][weight];
+                int take = INT_MIN;
+                if(wt[ind]<=weight)
+                take = val[ind] + dp[ind][weight-wt[ind]];
+           
+                dp[ind][weight] = max(notTake,take);
+            }
+        }
+        
+        return dp[N-1][W];
     }
 };
 
