54a61f1e2ead7647888a57d88aa0323bf89ab0a7
Shubham Kumar <87669020+shubhamkv@users.noreply.github.com>
2023-06-11 11:13:50 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -36,25 +36,48 @@ class Solution{
         
         // ------------------- Tabulation DP Solution-----------------
         
-        vector<vector<int>> dp(n, vector<int>(n+1,0));
+        // vector<vector<int>> dp(n, vector<int>(n+1,0));
+        
+        // for(int N=0;N<=n;N++)
+        //   dp[0][N] = N*price[0];
+          
+        // for(int ind=1;ind<n;ind++)
+        // {
+        //     for(int N=0;N<=n;N++)
+        //     {
+        //         int notTake = 0 + dp[ind-1][N];
+        //         int take = INT_MIN;
+        //         int rodlength = ind+1;
+        //         if(rodlength<=N)
+        //           take = price[ind] + dp[ind][N-rodlength];
+        
+        //         dp[ind][N] = max(take,notTake);
+        //     }
+        // }
+        //  return dp[n-1][n];
+        
+        // ---------------- Space Optimization-------------------------------
+        
+        vector<int> prev(n+1,0), curr(n+1,0);
         
         for(int N=0;N<=n;N++)
-          dp[0][N] = N*price[0];
+          prev[N] = N*price[0];
           
         for(int ind=1;ind<n;ind++)
         {
             for(int N=0;N<=n;N++)
             {
-                int notTake = 0 + dp[ind-1][N];
+                int notTake = 0 + prev[N];
                 int take = INT_MIN;
                 int rodlength = ind+1;
                 if(rodlength<=N)
-                  take = price[ind] + dp[ind][N-rodlength];
+                  take = price[ind] + curr[N-rodlength];
         
-                dp[ind][N] = max(take,notTake);
+                curr[N] = max(take,notTake);
             }
+            prev = curr;
         }
-         return dp[n-1][n];
+        return prev[n];
     }
 };
 
