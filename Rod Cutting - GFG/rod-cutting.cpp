56c9cce48ff462b39f51e074b2b4a656bfe7a3fd
Shubham Kumar <87669020+shubhamkv@users.noreply.github.com>
2023-06-11 11:11:46 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -9,28 +9,52 @@ using namespace std;
 // User function Template for C++
 
 class Solution{
-  private:
-  int f(int ind, int N, int price[], vector<vector<int>> &dp)
-  {
-      if(ind==0)
-        return N*price[0];
+    
+    // ------------------ Memoization DP Solution---------------------------
+//   private:
+//   int f(int ind, int N, int price[], vector<vector<int>> &dp)
+//   {
+//       if(ind==0)
+//         return N*price[0];
       
-      if(dp[ind][N]!=-1)
-        return dp[ind][N];
+//       if(dp[ind][N]!=-1)
+//         return dp[ind][N];
         
-      int notTake = 0 + f(ind-1,N,price,dp);
-      int take = INT_MIN;
-      int rodlength = ind+1;
-      if(rodlength<=N)
-        take = price[ind] + f(ind,N-rodlength,price,dp);
+//       int notTake = 0 + f(ind-1,N,price,dp);
+//       int take = INT_MIN;
+//       int rodlength = ind+1;
+//       if(rodlength<=N)
+//         take = price[ind] + f(ind,N-rodlength,price,dp);
         
-      return dp[ind][N] = max(take,notTake);
-  }
+//       return dp[ind][N] = max(take,notTake);
+//   }
   public:
     int cutRod(int price[], int n) {
         //code here
-        vector<vector<int>> dp(n, vector<int>(n+1,-1));
-        return f(n-1,n,price,dp);
+        // vector<vector<int>> dp(n, vector<int>(n+1,-1));
+        // return f(n-1,n,price,dp);
+        
+        // ------------------- Tabulation DP Solution-----------------
+        
+        vector<vector<int>> dp(n, vector<int>(n+1,0));
+        
+        for(int N=0;N<=n;N++)
+          dp[0][N] = N*price[0];
+          
+        for(int ind=1;ind<n;ind++)
+        {
+            for(int N=0;N<=n;N++)
+            {
+                int notTake = 0 + dp[ind-1][N];
+                int take = INT_MIN;
+                int rodlength = ind+1;
+                if(rodlength<=N)
+                  take = price[ind] + dp[ind][N-rodlength];
+        
+                dp[ind][N] = max(take,notTake);
+            }
+        }
+         return dp[n-1][n];
     }
 };
 
